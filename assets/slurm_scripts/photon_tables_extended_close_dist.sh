d7db3ad677fde6f9d9dd7a96ce25ef6be7da7d95
Christian Haack <christian.haack@tum.de>
2023-01-25 14:58:15 +0100

    fix typo

--- assets/slurm_scripts/photon_tables_extended_close_dist.sh
+++ assets/slurm_scripts/photon_tables_extended_close_dist.sh
@@ -13,6 +13,6 @@ n_skip=$((SLURM_ARRAY_TASK_ID * n_sims))
 n_resample=20
 
 start=$(date +%s)
-cd ~/repos/NeutrinoTelescopes/ && ~/.julia/juliaup/bin/julia --project=. scripts/photon_tables.jl --n_sims=${n_sims} --n_skip=${n_skip} --n_resample=${n_resample} --output data/photon_table_extended_${SLURM_ARRAY_TASK_ID}.hd5 --dist_min=1 --dist_max=50 --mode extended
+cd ~/repos/NeutrinoTelescopes/ && ~/.julia/juliaup/bin/julia --project=. scripts/photon_tables.jl --n_sims=${n_sims} --n_skip=${n_skip} --n_resample=${n_resample} --output data/photon_table_extended_close_dist_${SLURM_ARRAY_TASK_ID}.hd5 --dist_min=1 --dist_max=50 --mode extended
 end=$(date +%s)
 echo "Elapsed Time: $(($end-$start)) seconds"
