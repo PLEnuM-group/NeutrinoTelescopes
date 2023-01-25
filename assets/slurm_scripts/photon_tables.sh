c1e5b974af59a780a9f6d1cd056550599fa01fb2
Christian Haack <christian.haack@tum.de>
2023-01-25 11:34:32 +0100

    fix slurm script

--- assets/slurm_scripts/photon_tables.sh
+++ assets/slurm_scripts/photon_tables.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-
-#SBATCH --job-name=photon_tables                
-#SBATCH --ntasks=1                              
-#SBATCH --time=12:00:00                         
-#SBATCH --partition=lrz-v100x2
+#SBATCH -N 1
+#SBATCH --job-name=photon_tables
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --partition=lrz-v100x2,lrz-dgx-a100-80x8,lrz-dgx-1-p100x8
 #SBATCH --gres=gpu:1
+#SBATCH -o test.out
+#SBATCH -e test.err
 
-n_sims=1000
-n_skip = ${SLURM_ARRAY_TASK_ID} * $n_sims
-n_resample = 20
+n_sims=50
+n_skip=$((SLURM_ARRAY_TASK_ID * n_sims))
+n_resample=20
 
-cd ~/repos/NeutrinoTelescopes/ && ~/.julia/juliaup/bin/julia --project=. scripts/photon_tables.jl --n_sims=$n_sims --n_skip=$n_skip--n_resample=$n_resample --output data/photon_table_extended_${SLURM_ARRAY_TASK_ID}.hd5 --dist_min=5 --dist_max=200 --mode extended --seed ${SLURM_ARRAY_TASK_ID}
+start=$(date +%s)
+cd ~/repos/NeutrinoTelescopes/ && ~/.julia/juliaup/bin/julia --project=. scripts/photon_tables.jl --n_sims=${n_sims} --n_skip=${n_skip} --n_resample=${n_resample} --output data/photon_table_extended_${SLURM_ARRAY_TASK_ID}.hd5 --dist_min=5 --dist_max=200 --mode extended
+end=$(date +%s)
+echo "Elapsed Time: $(($end-$start)) seconds"
