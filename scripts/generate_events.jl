3159cc1710c1a01c88ff67d8daa452ce4a2165ed
Christian Haack <christian.haack@tum.de>
2023-03-20 09:58:08 +0100

    cleanup

--- scripts/generate_events.jl
+++ scripts/generate_events.jl
@@ -16,12 +16,12 @@ using Arrow
 BSON.@load joinpath(@__DIR__, "../data/extended_cascade_2_FNL.bson") model tf_vec
 
 
-targets_single = [make_pone_module(@SVector[-25., 0., -450.], 1)]
-targets_line = make_detector_line(@SVector[-25., 0.0, 0.0], 20, 50, 1)
+targets_single = [make_pone_module(@SVector[-25.0, 0.0, -450.0], 1)]
+targets_line = make_detector_line(@SVector[-25.0, 0.0, 0.0], 20, 50, 1)
 targets_three_l = [
-    make_detector_line(@SVector[-25., 0.0, 0.0], 20, 50, 1)
-    make_detector_line(@SVector[25., 0.0, 0.0], 20, 50, 21)
-    make_detector_line(@SVector[0., sqrt(50^2-25^2), 0.0], 20, 50, 41)]
+    make_detector_line(@SVector[-25.0, 0.0, 0.0], 20, 50, 1)
+    make_detector_line(@SVector[25.0, 0.0, 0.0], 20, 50, 21)
+    make_detector_line(@SVector[0.0, sqrt(50^2 - 25^2), 0.0], 20, 50, 41)]
 targets_hex = make_hex_detector(3, 50, 20, 50, truncate=1)
 
 medium = make_cascadia_medium_properties(0.99)
@@ -31,10 +31,10 @@ cylinder = get_bounding_cylinder(d)
 pdist = CategoricalSetDistribution(OrderedSet([PEMinus, PEPlus]), [0.5, 0.5])
 edist = Pareto(1, 1E4) + 1E4
 ang_dist = UniformAngularDistribution()
-length_dist = Dirac(0.)
-time_dist = Dirac(0.)
-inj = VolumeInjector(cylinder, edist, pdist, ang_dist,length_dist, time_dist)
-hit_generator = SurrogateModelHitGenerator(model, tf_vec, 200., d)
+length_dist = Dirac(0.0)
+time_dist = Dirac(0.0)
+inj = VolumeInjector(cylinder, edist, pdist, ang_dist, length_dist, time_dist)
+hit_generator = SurrogateModelHitGenerator(model, tf_vec, 200.0, d)
 ec = EventCollection(inj)
 
 event = rand(inj)
@@ -56,4 +56,3 @@ open("test.arrow", "w") do io
     end
 end
 JSON3.write(ec)
-
