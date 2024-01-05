39febc7863e324fba1c053c85431fc625713b255
Christian Haack <chr.hck@gmail.come>
2024-01-05 16:37:27 +0100

    Add medium perturbation option to train_time_surrogate.jl

--- scripts/surrogate_models/train_time_surrogate.jl
+++ scripts/surrogate_models/train_time_surrogate.jl
@@ -35,6 +35,9 @@ s = ArgParseSettings()
     "--model_name"
     help = "Model name"
     required = true
+    "--perturb_medium"
+    help = "Train a model with medium perturbation"
+    action = :store_true
 end
 parsed_args = parse_args(ARGS, s; as_symbols=true)
 
@@ -45,7 +48,10 @@ model_name = parsed_args[:model_name]
 
 hit_buffer = Vector{Float64}(undef, Int64(1E8))
 pmt_ixs_buffer = Vector{Int64}(undef, Int64(1E8))
-features_buffer = Matrix{Float64}(undef, 24, Int64(1E8))
+
+feature_length = parsed_args[:perturb_medium] ? 24 + 2 : 24
+
+features_buffer = Matrix{Float64}(undef, feature_length, Int64(1E8))
 
 rng = MersenneTwister(31338)
 nsel_frac = 0.9
@@ -59,7 +65,10 @@ end
 
 data = (tres=hits, label=features)
 
-hparams = RQNormFlowHParams(
+
+model_type = parsed_args[:perturb_medium] ? AbsScaRQNormFlowHParams : RQNormFlowHParams
+
+hparams = model_type(
     K=12,
     batch_size=5000,
     mlp_layers = 2,
