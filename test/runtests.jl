0518743a84ea663d77540e2bc8b0deb7e034bb28
Christian Haack <chr.hck@gmail.come>
2024-05-19 09:36:00 +0200

    refactoring

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,42 +9,6 @@ using CUDA
 using QuadGK
 
 @testset "NeutrinoTelescopes.jl" begin
-    @testset "SurrogateModels" begin
-        @testset "NormalizingFlow" begin
-
-            num_bins = 10
-            x = -10:0.1:10
-            params = randn(3 * num_bins + 1,  length(x))
-            x_pos, y_pos, knot_slopes = constrain_spline_params(params, -5, 5)
-            y, logdet = rqs_univariate(x_pos, y_pos, knot_slopes, x)
-            xrt, logdet_inv = inv_rqs_univariate(x_pos, y_pos, knot_slopes, y)
-
-            @test all(logdet .≈ -logdet_inv)
-            @test all(isapprox.(x, xrt; atol=1E-5))
-
-            x = CuArray(x)
-            params = CuMatrix(params)
-            x_pos, y_pos, knot_slopes = constrain_spline_params(params, -5, 5)
-            y, logdet = rqs_univariate(x_pos, y_pos, knot_slopes, x)
-            xrt, logdet_inv = inv_rqs_univariate(x_pos, y_pos, knot_slopes, y)
-
-            @test all(Vector(logdet) .≈ -Vector(logdet_inv))
-            @test all(isapprox.(Vector(x), Vector(xrt); atol=1E-5))
-
-
-            p = randn(3 * num_bins + 1 + 2)
-            p[end] = abs(p[end])
-
-            function integrand(x)
-                x = vec([x])                
-                lpdf = eval_transformed_normal_logpdf(x, p, -5, 5)
-                return exp(lpdf[1])
-            end
-
-            @test isapprox(quadgk(integrand, -10, 10)[1], 1, atol=1E-5)
-
-        end
-    end
     @testset "LightYield" begin
         @testset "Sources" begin
             src = PointlikeTimeRangeEmitter(
